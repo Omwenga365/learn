b90d90ab62d38244bccf8753c54d1ec59eaa8232
Omwenga365 <victoromwenga365@gmail.com>
2023-12-21 23:30:39 +0300

    first commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,23 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+    return <div>
+       <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+      <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum fuga pariatur necessitatibus ex maxime ut minima sed modi sit id, earum deserunt voluptas laboriosam error accusamus enim aperiam esse!</Typography>
+      <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magni maxime id quia nobis voluptas delectus, cupiditate assumenda autem excepturi adipisci animi, iusto mollitia rem dolores ad harum a eius? </Typography>
+      <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum fuga pariatur necessitatibus ex maxime ut minima sed modi sit id, earum deserunt voluptas laboriosam error accusamus enim aperiam esse!</Typography>
+      <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magni maxime id quia nobis voluptas delectus, cupiditate assumenda autem excepturi adipisci animi, iusto mollitia rem dolores ad harum a eius? </Typography>
+
+
+    </div>
+}
+
+
+
